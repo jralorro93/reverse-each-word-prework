03d196f10217401bf078a11a90bf53b9de6dd2fd
Juan Alorro <jralorro93@gmail.com>
2019-02-03 21:55:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(phrase)
-  new_array = phrase.split 
-  new_phrase = []
-  new_array.each do |word|
-    new_phrase << word.reverse!
-  end 
-  new_phrase.join(" ")
-end 
+# def reverse_each_word(phrase)
+#   new_array = phrase.split 
+#   new_phrase = []
+#   new_array.each do |word|
+#     new_phrase << word.reverse!
+#   end 
+#   new_phrase.join(" ")
+# end 
